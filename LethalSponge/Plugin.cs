7aa945095375b540598641c0e828cea7f0a88b56
Scoops <cassidy@coffeyhouse.com>
2025-06-01 13:14:45 -0400

    Version bump for v70

--- LethalSponge/Plugin.cs
+++ LethalSponge/Plugin.cs
@@ -19,7 +19,7 @@ public static class PluginInformation
 {
     public const string PLUGIN_GUID = "LethalSponge";
     public const string PLUGIN_NAME = "LethalSponge";
-    public const string PLUGIN_VERSION = "1.1.9";
+    public const string PLUGIN_VERSION = "1.1.10";
 }
 
 [BepInPlugin(PluginInformation.PLUGIN_GUID, PluginInformation.PLUGIN_NAME, PluginInformation.PLUGIN_VERSION)]
